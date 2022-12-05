8bd3322284405c05ac310cba2b03a740b4ecc7d4
Svahnen <svahn.jonny@gmail.com>
2022-12-05 11:39:25 +0100

    Upload iot_configs.h example file

---
+++ iot_configs_example.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "XXXXXXXXXX"
+#define IOT_CONFIG_WIFI_PASSWORD "XXXXXXXXXX"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "XXXXXXXXXX.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "XXXXXXXXXX"
+#define IOT_CONFIG_DEVICE_KEY "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+// Publish 1 message every X milliseconds
+#define TELEMETRY_FREQUENCY_MILLISECS 60000
